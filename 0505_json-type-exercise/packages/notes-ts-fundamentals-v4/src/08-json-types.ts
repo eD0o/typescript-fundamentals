0b7185b9972774517834f4e814dd9792e6c44668
Eduardo <eduardo.benigno15@gmail.com>
2024-10-13 10:19:05 -0300

    feat: finishing class 0505

--- 0505_json-type-exercise/packages/notes-ts-fundamentals-v4/src/08-json-types.ts
+++ 0505_json-type-exercise/packages/notes-ts-fundamentals-v4/src/08-json-types.ts
@@ -1,46 +1,48 @@
-//* JSON types
-/*
-A JSON value MUST be an
-  · object
-  · array
-  · number
-  · string,
-or one of the following three literal names:
-  · false
-  · true
-  · null
-*/
-/**
- * A JSON object type   {    }
- */
-type JSONObject = any
-/**
- * A JSON array type   [    ]
- */
-type JSONArray = any
-/**
- * A type representing any valid JSON value
- */
-type JSONValue = any
+// //* JSON types
+// /*
+// A JSON value MUST be an
+//   · object
+//   · array
+//   · number
+//   · string,
+// or one of the following three literal names:
+//   · false
+//   · true
+//   · null
+// */
 
-//! DO NOT EDIT ANY CODE BELOW THIS LINE
-function isJSON(arg: JSONValue) {}
+// type JSONPrimitve = string | number | boolean | null
+// /**
+//  * A JSON object type   {    }
+//  */
+// type JSONObject = { [key: string]: JSONValue }
+// /**
+//  * A JSON array type   [    ]
+//  */
+// type JSONArray = JSONValue[]
+// /**
+//  * A type representing any valid JSON value
+//  */
+// type JSONValue = JSONPrimitve | JSONObject | JSONArray
 
-//✔️ POSITIVE test cases (must pass)
-isJSON('hello') //✔️ Strings
-isJSON([4, 8, 15, 16, 23, 42]) //✔️ Arrays of numbers
-isJSON({ greeting: 'hello' }) //✔️ Objects
-isJSON(false) //✔️ Boolean values
-isJSON(true)
-isJSON(null) //✔️ null values
-isJSON({ a: { b: [2, 3, 'foo', null, false] } }) //✔️ A complex object
+// //! DO NOT EDIT ANY CODE BELOW THIS LINE
+// function isJSON(arg: JSONValue) { }
 
-//! NEGATIVE test cases (must fail)
-//// @ts-expect-error
-isJSON(() => '') //! Functions are not valid JSON
-//// @ts-expect-error
-isJSON(class {}) //! Classes are not valid JSON
-//// @ts-expect-error
-isJSON(undefined) //! undefined is not valid JSON
-//// @ts-expect-error
-isJSON(BigInt(143)) //! BigInts are not valid JSON
+// //✔️ POSITIVE test cases (must pass)
+// isJSON('hello')
+// isJSON([4, 8, 15, 16, 23, 42])
+// isJSON({ greeting: 'hello' })
+// isJSON(false)
+// isJSON(true)
+// isJSON(null)
+// isJSON({ a: { b: [2, 3, 'foo', null, false] } })
+
+// //! NEGATIVE test cases (must fail)
+// //// @ts-expect-error
+// isJSON(() => '') //! Functions are not valid JSON
+// //// @ts-expect-error
+// isJSON(class { }) //! Classes are not valid JSON
+// //// @ts-expect-error
+// isJSON(undefined) //! undefined is not valid JSON
+// //// @ts-expect-error
+// isJSON(BigInt(143)) //! BigInts are not valid JSON
